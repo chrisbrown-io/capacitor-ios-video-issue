e75d0d6311aa59b80e8bcdfc8aefd61e49527410
Chris Brown <chris@chrisbrown.io>
2023-08-04 15:07:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# capacitor-ios-video-issue
+
+Repro for <https://github.com/ionic-team/capacitor/issues/6790>
+
+## Setup
+
+```
+npm i
+
+npm run sync
+
+npm run open:ios
+```
+
+Run build on device using xcode
---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "appId": "com.parallel.capacitoriosvideoissue",
+  "appName": "capacitor-ios-video-issue",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https"
+    // "url": "http://<your_ip>:8080"
+  }
+}
